da98f71f1a5151d8e684d4a9c1b1a94ff3bce3e8
Chacha <shuang5912@163.com>
2017-08-08 16:52:35 +0800

    机器学习
    2017/08/08 decisionTree

--- decisionTree/test.py
+++ decisionTree/test.py
@@ -1,3 +1,11 @@
 import trees
-myDat,labels = trees.createDataSet()
-print trees.calcShannonEnt(myDat)
+import treePlotter
+# myDat,labels = trees.createDataSet()
+# myTree = trees.createTree(myDat, labels)
+# treePlotter.createPlot(myTree)
+# trees.storageTree(myTree, 'classifierStorage.txt')
+fr = open('lenses.txt')
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels = ['age','prescript', 'astigmatic', 'treatRate']
+lensesTree = trees.createTree(lenses, lensesLabels)
+treePlotter.createPlot(lensesTree)
--- decisionTree/trees.py
+++ decisionTree/trees.py
@@ -1,4 +1,5 @@
-from math import  log
+from math import log
+import operator
 
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
@@ -23,3 +24,68 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntries
         shannonEnt -= prob*log(prob,2)
     return shannonEnt
+
+def splitDateSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0])-1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDateSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob*calcShannonEnt(subDataSet)
+        infoGain = baseEntropy-newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0.0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter[1], reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(classList) == 1:
+        return majorityCnt(classList)
+    bestFeature = chooseBestFeatureToSplit(dataSet)
+    bestFeatureLabel = labels[bestFeature]
+    myTree = {bestFeatureLabel:{}}
+    del(labels[bestFeature])
+    featValue = [example[bestFeature] for example in dataSet]
+    uniqueVals = set(featValue)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatureLabel][value] = createTree(splitDateSet(dataSet, bestFeature, value), subLabels)
+    return myTree
+
+def storageTree(inputTree, fileName):
+    import pickle
+    fw = open(fileName, 'w')
+    pickle.dump(inputTree, fw)
+    fw.close()
+
+def grabTree(fileName):
+    import pickle
+    fr = open(fileName)
+    return pickle.load(fr)
